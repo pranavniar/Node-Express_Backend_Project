8af3ea678ccd8f7129d37c3b210260b0e54e0a20
pranavniar <pranavnairdemon@gmail.com>
2024-04-07 23:45:13 +0530

    changes in allowedOrigins and changed what is being retured by the authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,62 +1,56 @@
 const User = require("../model/User");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const handleLogin = async (req, res) => {
-    const { user, password } = req.body;
-    if (!user || !password) {
-      return res
-        .status(400)
-        .json({ message: "User and password are required" });
-    }
-    const foundUser = await User.findOne({ username: user }).exec();
-    if (!foundUser) {
-      return res.status(401).json({ message: "User not found" });
-    }
-    try {
-      if (await bcrypt.compare(password, foundUser.password)) {
-        
-        const roles = Object.values(foundUser.roles);
-        // Create the access token
-        const accessToken = jwt.sign(
-          {
-            "UserInfo":{
-              "username": foundUser.username,
-              "roles": roles
-            }
-          },
-          process.env.ACCESS_TOKEN_SECRET,
-          { expiresIn: "60s" }
-        );
-        
-        // Create the refresh token
-        const refreshToken = jwt.sign(
-          { username: foundUser.username },
-          process.env.REFRESH_TOKEN_SECRET,
-          { expiresIn: "1d" }
-        );
-
-        // Save the refresh token with current user in the database to create a logout route
-        //that invalidates the refresh token when the user logs out
-        foundUser.refreshToken = refreshToken;
-        result = await foundUser.save();
-        console.log(result);
-        
-        res.cookie('jwt', refreshToken, 
+  const { user, password } = req.body;
+  if (!user || !password) {
+    return res.status(400).json({ message: "User and password are required" });
+  }
+  const foundUser = await User.findOne({ username: user }).exec();
+  if (!foundUser) {
+    return res.status(401).json({ message: "User not found" });
+  }
+  try {
+    if (await bcrypt.compare(password, foundUser.password)) {
+      const roles = Object.values(foundUser.roles).filter(Boolean);
+      // Create the access token
+      const accessToken = jwt.sign(
         {
-          httpOnly: true,
-          sameSite:'None',
-          maxAge: 24*60*60*1000
-        });
-        res.status(200).json({accessToken});
-      } else {
-        res.status(401).json({ message: "Invalid password" });
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ message: err.message });
+          UserInfo: {
+            username: foundUser.username,
+            roles: roles,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "60s" }
+      );
+
+      // Create the refresh token
+      const refreshToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+
+      // Save the refresh token with current user in the database to create a logout route
+      //that invalidates the refresh token when the user logs out
+      foundUser.refreshToken = refreshToken;
+      result = await foundUser.save();
+
+      res.cookie("jwt", refreshToken, {
+        httpOnly: true,
+        sameSite: "None",
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      res.status(200).json({ accessToken, roles });
+    } else {
+      res.status(401).json({ message: "Invalid password" });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = { handleLogin };
--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,24 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if(!authHeader) {
-        return res.status(401).json({message: "Token is required"});
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Token is required" });
+  }
+  console.log(authHeader); //Bearer token
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
     }
-    console.log(authHeader); //Bearer: token
-    const token = authHeader.split(" ")[1];
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        (err, decoded)=>{
-            if(err) {
-                return res.status(403).json({message: "Invalid token"});
-            }
-            req.user = decoded.UserInfo.username;
-            req.roles = decoded.UserInfo.roles;
-            next();
-        }
-    );
+    req.user = decoded.UserInfo.username;
+    req.roles = decoded.UserInfo.roles;
+    next();
+  });
 };
 
 module.exports = verifyJWT;
